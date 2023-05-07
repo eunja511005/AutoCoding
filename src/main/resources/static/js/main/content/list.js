4d40bac3c534c4f4225001d9014c8034a974a608
eunja511005 <eunja511005@gmail.com>
2023-05-08 00:17:44 +0900

    :pencil2: [성능] 써머노트에서 이미지 업로드시 압축 해서 업로드 하도록 변경 #6  Changes to be committed: 	modified:   src/main/resources/static/js/main/content/list.js

--- src/main/resources/static/js/main/content/list.js
+++ src/main/resources/static/js/main/content/list.js
@@ -43,18 +43,20 @@ $(document).ready(function() {
             },
             {"data": "id",
          	   "render": function(data, type, row, meta){
-         		   //return '<button type="button" class="btn btn-primary">View Content</button>';
+         		   // return '<button type="button" class="btn
+					// btn-primary">View Content</button>';
          		   return '<i class="fa-solid fa-magnifying-glass-chart"></i>';
          	   }                       
             },
             {"data": "",
          	   "render": function(data, type, row, meta){
-                    //return '<button type="button" class="btn btn-danger">Delete</button>';
+                    // return '<button type="button" class="btn
+					// btn-danger">Delete</button>';
                     return '<i class="fa-solid fa-trash-can"></i>';
                 }
             }
         ],
-        order: [[3, 'desc']]
+        order: [[2, 'desc']]
     });
 
     $('#search_btn').on('click', function () {
@@ -64,7 +66,7 @@ $(document).ready(function() {
     
     $('#newPostModalButton').click(function() {
         $('#newPostModal').modal('show');
-    });
+    });   
     
 	$('#modalContent').summernote({
   	    placeholder: 'Hello Summernote lite',
@@ -72,7 +74,8 @@ $(document).ready(function() {
         height: 300,                 // set editor height
         minHeight: null,             // set minimum height of editor
         maxHeight: null,             // set maximum height of editor
-        focus: true,                  // set focus to editable area after initializing summernote
+        focus: true,                  // set focus to editable area after
+										// initializing summernote
         toolbar: [
   		    ['fontname', ['fontname']],
   		    ['fontsize', ['fontsize']],
@@ -83,8 +86,11 @@ $(document).ready(function() {
   		    ['height', ['height']],
   		    ['insert',['picture','link','video']],
   		    ['view', ['codeview','fullscreen', 'help']]
-  		  ],
-  		  // 추가한 글꼴
+  		  ],		  
+		  callbacks: {
+		      onImageUpload: onImageUpload
+		  },  		  
+  		// 추가한 글꼴
   		fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋음체','바탕체'],
   		 // 추가한 폰트사이즈
   		fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
@@ -113,7 +119,10 @@ $(document).ready(function() {
     $(document).on('click', '.fa-magnifying-glass-chart', function(){ 
         var $btn=$(this);
         var $tr=$btn.closest('tr');
-        var dataTableRow=$("#example").DataTable().row($tr[0]); // get the DT row so we can use the API on it
+        var dataTableRow=$("#example").DataTable().row($tr[0]); // get the DT
+																// row so we can
+																// use the API
+																// on it
         var rowData=dataTableRow.data();
         var id = rowData.id;
           $.ajax({
@@ -141,7 +150,7 @@ $(document).ready(function() {
               	}
               },
               error : function (jqXHR){
-          		console.log(jqXHR);  //응답 메시지
+          		console.log(jqXHR);  // 응답 메시지
           	}
           });	                
         
@@ -181,7 +190,7 @@ $(document).ready(function() {
 	                    	}
 	                    },
 	                    error : function (jqXHR){
-	                		console.log(jqXHR);  //응답 메시지
+	                		console.log(jqXHR);  // 응답 메시지
 	                	}
 	                });
     		  } else {
@@ -240,7 +249,7 @@ function save(form){
 	            });			
 		},
 		error: function(jqXHR) {
-			console.log(jqXHR);  //응답 메시지
+			console.log(jqXHR);  // 응답 메시지
 			swal({
 				title: "Registration failed",
 				text: "Error : " + jqXHR.responseJSON.message,
@@ -259,3 +268,65 @@ function save(form){
 	});	
 }
 
+// onImageUpload callback 함수 정의
+function onImageUpload(files) {
+	debugger;
+    for (var i = 0; i < files.length; i++) {
+    	
+    	// 이미지 파일을 압축합니다. (압축률: 50%)
+    	compressImage(files[i], 200, 0.5).then(function (compressedImage) {
+    	    // 압축된 이미지 파일을 써머노트 본문에 삽입합니다.
+    	    const reader = new FileReader();
+    	    reader.onload = function (e) {
+    	      const imgNode = $('<img>').attr('src', e.target.result);
+    	      $('#modalContent').summernote('insertNode', imgNode[0]);
+    	    }
+    		reader.readAsDataURL(compressedImage);
+    	});
+    }
+}
+
+// 이미지 파일을 압축하는 함수
+function compressImage(imageFile, maxSize, quality) {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const reader = new FileReader();
+    reader.readAsDataURL(imageFile);
+    reader.onload = function (e) {
+    const img = new Image();
+    img.src = e.target.result;
+    img.onload = function () {
+     	// Calculate new dimensions based on max size parameter
+    	let newWidth = img.width;
+    	let newHeight = img.height;
+    	if (newWidth > newHeight) {
+    	   if (newWidth > maxSize) {
+    	        newHeight *= maxSize / newWidth;
+    	        newWidth = maxSize;
+    	   }
+    	} else {
+    	   if (newHeight > maxSize) {
+    	        newWidth *= maxSize / newHeight;
+    	        newHeight = maxSize;
+    	      }
+    	}
+    	
+    	// 캔버스에 이미지를 그립니다.
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        ctx.drawImage(img, 0, 0, newWidth, newHeight);
+
+        // 캔버스에서 이미지 데이터를 가져옵니다.
+        canvas.toBlob((blob) => {
+        	console.log("Original Size:",imageFile.size);
+            console.log("Compressed image size:", blob.size);
+          // 압축된 이미지 파일을 반환합니다.
+          resolve(blob);
+        }, imageFile.type, quality);
+      }
+    };
+    reader.onerror = reject;
+  });
+}
