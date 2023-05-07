9734fbc0f9002b46cd5d3346473e69d4f6095385
eunja511005 <eunja511005@gmail.com>
2023-05-07 11:29:15 +0900

    메인 클래스 gitignore에서 제외

---
+++ src/main/java/com/eun/tutorial/TutorialApplication.java
@@ -0,0 +1,22 @@
+package com.eun.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class TutorialApplication extends SpringBootServletInitializer{
+//public class TutorialApplication {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TutorialApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApplication.class, args);
+	}
+
+}
