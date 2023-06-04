5f438d1e2d3e06975931beaa953ac24d08afa90d
eunja511005 <eunja511005@gmail.com>
2023-06-04 13:49:37 +0900

    :pencil2: [사용] Anonymous 로그인(로그인 안한 상태) 테스트 코드 추가 #49  Changes to be committed: 	modified:   src/test/java/com/eun/tutorial/service/main/MenuServiceImplTest.java

--- src/test/java/com/eun/tutorial/service/main/MenuServiceImplTest.java
+++ src/test/java/com/eun/tutorial/service/main/MenuServiceImplTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -20,15 +21,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import com.eun.tutorial.dto.UserInfoDTO;
 import com.eun.tutorial.dto.main.MenuDTO;
@@ -45,6 +44,9 @@ class MenuServiceImplTest {
 
 	@Mock
 	private MessageSource messageSource;
+	
+    @Mock
+    private SecurityContext securityContextMock;
 
 	@BeforeEach
 	void setUp() {
@@ -54,7 +56,7 @@ class MenuServiceImplTest {
 	}
 
 	@Test
-	void generateMenuHtml_withUserRole() {
+	void generateMenuHtml_withSysRole() {
 
 		// Arrange
 		Authentication authentication = Mockito.mock(Authentication.class);
@@ -69,16 +71,61 @@ class MenuServiceImplTest {
 		
 		// Assert
 		String expectedMenu = "	<div class=\"sb-sidenav-menu-heading\">Core</div>\n"
-		        + "	<a class=\"nav-link a-menu\" href=\"/posts/list\">\n"
-		        + "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-comments\"></i></div>오픈포럼\n"
-		        + "	</a>\n"
-		        + "	<a class=\"nav-link a-menu\" href=\"/relationShip/list\">\n"
-		        + "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-handshake\"></i></div>관계\n"
-		        + "	</a>\n"
-		        + "	<div class=\"sb-sidenav-menu-heading\">Admin</div>\n"
-		        + "	<a class=\"nav-link a-menu\" href=\"#\">\n"
-		        + "		<div class=\"sb-nav-link-icon\"><i class=\"fa fa-user-shield\"></i></div>관리자\n"
-		        + "	</a>\n";
+				+ "	<a class=\"nav-link a-menu\" href=\"/posts/list\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-comments\"></i></div>오픈포럼\n"
+				+ "	</a>\n"
+				+ "	<a class=\"nav-link a-menu\" href=\"/relationShip/list\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-handshake\"></i></div>관계\n"
+				+ "	</a>\n"
+				+ "	<div class=\"sb-sidenav-menu-heading\">Admin</div>\n"
+				+ "	<a class=\"nav-link a-menu\" href=\"#\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"fa fa-user-shield\"></i></div>관리자\n"
+				+ "	</a>\n"
+				+ "	<div class=\"sb-sidenav-menu-heading\">Interface</div>\n"
+				+ "	<a class=\"nav-link collapsed\" data-bs-toggle=\"collapse\" data-bs-target=\"#collapselayout\" aria-expanded=\"false\" aria-controls=\"collapselayout\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-columns\"></i></div>레이아웃\n"
+				+ "		<div class=\"sb-sidenav-collapse-arrow\"><i class=\"fas fa-angle-down\"></i></div>\n"
+				+ "	</a>\n"
+				+ "	<div class=\"collapse\" id=\"collapselayout\" aria-labelledby=\"headinglayout\" data-bs-parent=\"#sidenavAccordionroot\">\n"
+				+ "		<nav class=\"sb-sidenav-menu-nested nav accordion\" id=\"sidenavAccordionlayout\">\n"
+				+ "	<a class=\"nav-link a-menu\" href=\"layout-static.html\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"N/A\"></i></div>고정 내비게이션\n"
+				+ "	</a>\n"
+				+ "	<a class=\"nav-link a-menu\" href=\"layout-sidenav-light.html\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"N/A\"></i></div>라이트 사이드네비게이션\n"
+				+ "	</a>\n"
+				+ "		</nav>\n"
+				+ "	</div>\n";
+		
+		assertEquals(expectedMenu, actualMenu);
+		verify(menuMapper, times(1)).selectMenuList();
+	}
+	
+	@Test
+	void generateMenuHtml_withNoLogin() {
+		// Arrange
+		// 1) Create the Granted Authority for the anonymous user
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ANONYMOUS");
+
+        // 2) Create the Authentication object for the anonymous user
+        Authentication anonymousAuthentication = new AnonymousAuthenticationToken(
+                "key", // A unique key for the anonymous user
+                "anonymousUser", // The username for the anonymous user
+                Collections.singletonList(authority)
+        );
+        
+        // Mock the SecurityContext
+        when(securityContextMock.getAuthentication()).thenReturn(anonymousAuthentication);
+        SecurityContextHolder.setContext(securityContextMock);
+
+		// Act
+		String actualMenu = menuService.generateMenuHtml();
+		
+		// Assert
+		String expectedMenu = "	<div class=\"sb-sidenav-menu-heading\">Core</div>\n"
+				+ "	<a class=\"nav-link a-menu\" href=\"/posts/list\">\n"
+				+ "		<div class=\"sb-nav-link-icon\"><i class=\"fas fa-comments\"></i></div>오픈포럼\n"
+				+ "	</a>\n";
 		
 		assertEquals(expectedMenu, actualMenu);
 		verify(menuMapper, times(1)).selectMenuList();
@@ -103,6 +150,15 @@ class MenuServiceImplTest {
                 	case "admin":
                 		returnMessage = "관리자";	
                 		break;
+                	case "layout":
+                		returnMessage = "레이아웃";	
+                		break;
+                	case "staticNavigation":
+                		returnMessage = "고정 내비게이션";	
+                		break;
+                	case "lightSidenav":
+                		returnMessage = "라이트 사이드네비게이션";	
+                		break;
             	}
             }else if (locale.getLanguage().equals(Locale.ENGLISH.getLanguage())) {
             	switch (code) {
@@ -115,6 +171,15 @@ class MenuServiceImplTest {
             	case "admin":
             		returnMessage = "admin";	
             		break;
+            	case "layout":
+            		returnMessage = "layout";	
+            		break;
+            	case "staticNavigation":
+            		returnMessage = "staticNavigation";	
+            		break;
+            	case "lightSidenav":
+            		returnMessage = "lightSidenav";	
+            		break;
         	}
         }
             return returnMessage;
@@ -123,12 +188,12 @@ class MenuServiceImplTest {
 
 	private void setMenuMock() {
 		List<MenuDTO> expectedMenuList = new ArrayList<>();
-		expectedMenuList.add(createMenu("menu_1", "Core", 1, "openForum", "/posts/list", "fas fa-comments", 1, "root",
-				false, "ROLE_ANY"));
-		expectedMenuList.add(createMenu("menu_2", "Core", 1, "relation", "/relationShip/list", "fas fa-handshake", 2,
-				"root", false, "ROLE_ANY"));
-		expectedMenuList
-				.add(createMenu("menu_3", "Admin", 1, "admin", "#", "fa fa-user-shield", 3, "root", false, "ROLE_SYS"));
+		expectedMenuList.add(createMenu("menu_1", "Core", 1, "openForum", "/posts/list", "fas fa-comments", 1, "root", false, "ROLE_ANY"));
+		expectedMenuList.add(createMenu("menu_2", "Core", 1, "relation", "/relationShip/list", "fas fa-handshake", 2, "root", false, "ROLE_USER"));
+		expectedMenuList.add(createMenu("menu_3", "Admin", 1, "admin", "#", "fa fa-user-shield", 3, "root", false, "ROLE_SYS"));
+		expectedMenuList.add(createMenu("menu_4", "Interface", 1, "layout", "#", "fas fa-columns", 4, "root", false, "ROLE_USER"));
+		expectedMenuList.add(createMenu("menu_5", "Interface", 2, "staticNavigation", "layout-static.html", "N/A", 1, "layout", false, "ROLE_USER"));
+		expectedMenuList.add(createMenu("menu_6", "Interface", 2, "lightSidenav", "layout-sidenav-light.html", "N/A", 2, "layout", false, "ROLE_USER"));
 		when(menuMapper.selectMenuList()).thenReturn(expectedMenuList);
 	}
 
