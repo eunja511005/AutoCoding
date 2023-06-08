2085cec7ecdcb0dba17eefce369214d3c51fff8a
eunja511005 <eunja511005@gmail.com>
2023-06-08 18:56:45 +0900

    :pencil2: 매일 밤 12시에 7일 지난 에러 데이터 삭제 배치 시작 #61  Changes to be committed: 	modified:   src/main/java/com/eun/tutorial/batch/BatchScheduler.java

--- src/main/java/com/eun/tutorial/batch/BatchScheduler.java
+++ src/main/java/com/eun/tutorial/batch/BatchScheduler.java
@@ -28,6 +28,7 @@ public class BatchScheduler {
      * 초(0-59) 분(0-59) 시간(0-23) 일(1-31) 월(1-12) 요일(0-7)
      */
     //@Scheduled(cron = "0/60 * * * * ?")
+	@Scheduled(cron = "0 0 0 * * ?")
     public void deleteBatchForZthhError() {
     	printStart("[Batch] Delete ZTHH_ERROR");
     }
