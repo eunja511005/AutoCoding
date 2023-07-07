1095df9961dbe1c326d3690040df828946491780
eunja511005 <eunja511005@gmail.com>
2023-07-08 08:44:21 +0900

    :new: [확장] jsonString 입력시 DTO 생성해 주는 유틸 개발  Changes to be committed: 	new file:   src/main/java/com/eun/tutorial/util/JsonStringToDTOSourceUtils.java 	modified:   src/main/java/com/eun/tutorial/util/StringUtils.java

--- src/main/java/com/eun/tutorial/util/StringUtils.java
+++ src/main/java/com/eun/tutorial/util/StringUtils.java
@@ -4,4 +4,20 @@ public class StringUtils {
     public static boolean isBlank(String str) {
         return str == null || str.isEmpty();
     }
+    
+    // 입력값의 첫번째 문자 대문자로 변환
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+    
+    // 입력값의 첫번째 문자 소문자로 변화
+    public static String lowercaseFirstCharacter(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toLowerCase(str.charAt(0)) + str.substring(1);
+    }
 }
